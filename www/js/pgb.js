e3e10e3f0187d465d5b9a4d634511b59072a50e8
konop93 <kasiakonopelska@gmail.com>
2017-03-28 00:03:35 +0200

    ruch telefonu

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -6,19 +6,19 @@ function onDeviceReady() {
 	navigator.notification.beep(1);
 }
 
-function checkConnection() {
-    var networkState = navigator.connection.type;
-
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = '2G telephone network';
-    states[Connection.CELL_3G]  = '3G telephone network';
-    states[Connection.CELL_4G]  = '4G telephone network';
-    states[Connection.CELL]     = 'Telephone network';
-    states[Connection.NONE]     = 'No connection';
-
-    navigator.notification.alert('Connection type: ' + states[networkState]);
+function motion() {
+	
+	function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
 }
+
+function onError() {
+    alert('onError!');
+}
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+	
 }
